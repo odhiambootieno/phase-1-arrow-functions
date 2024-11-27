a28b093787865830d5ee67de34fa2608985f4485
odhiambootieno <brianmkcnight@gmail.com>
2024-11-27 21:17:19 +0300

    adds

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression for dividing two numbers
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  // Arrow function for squaring a number
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function for adding two numbers
+  const add = (a, b) => {
+    return a + b;
+  };
